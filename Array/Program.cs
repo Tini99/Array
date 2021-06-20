9552aed7d9f20951f0ee5b4cc9b7bbc980c48d84
Tini Gisa <1814759@nzse.ac.nz>
2021-06-20 14:46:29 +1200

    Add project files.

---
+++ Array/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] balance = new int[5];
+            int i; 
+
+            for(i = 0; i < 5; i++)
+            {
+                int number = i + 1;
+                Console.WriteLine("Enter the balance of " + number);
+                balance[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.Write("The ToTal is =");
+            Console.WriteLine(balance[3]);
+        }
+    }
+}
